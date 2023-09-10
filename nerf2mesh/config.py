1c9926e7650de5d4930f065a4633a5b4e03afec2
Mohit Motwani <mohitmotwani54@gmail.com>
2023-09-10 11:09:09 +0000

    Added configurtation for initial execution

--- nerf2mesh/config.py
+++ nerf2mesh/config.py
@@ -1,19 +1,18 @@
 from nerfstudio.plugins.types import MethodSpecification
 from nerfstudio.engine.trainer import TrainerConfig
 from nerfstudio.engine.optimizers import AdamOptimizerConfig
-from nerfstudio.pipelines.base_pipeline import VanillaPipelineConfig
 from nerfstudio.pipelines.dynamic_batch import DynamicBatchPipelineConfig
 from nerfstudio.data.datamanagers.base_datamanager import (
-    VanillaDataManager,
     VanillaDataManagerConfig,
 )
-from nerfstudio.data.dataparsers.instant_ngp_dataparser import InstantNGPDataParserConfig
-
-from nerfstudio.models.instant_ngp import InstantNGPModelConfig
+from nerfstudio.data.dataparsers.instant_ngp_dataparser import (
+    InstantNGPDataParserConfig,
+)
+from nerf2mesh.nerf2mesh import Nerf2MeshModelConfig
 
 nerf2mesh = MethodSpecification(
     config=TrainerConfig(
-        method_name="instant-ngp",
+        method_name="nerf2mesh",
         steps_per_eval_batch=500,
         steps_per_save=2000,
         max_num_iterations=30000,
@@ -24,16 +23,18 @@ nerf2mesh = MethodSpecification(
                 train_num_rays_per_batch=4096,
                 eval_num_rays_per_batch=4096,
             ),
-            model=InstantNGPModelConfig(eval_num_rays_per_chunk=8192),
+            model=Nerf2MeshModelConfig(eval_num_rays_per_chunk=8192),
         ),
         optimizers={
             "fields": {
                 "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
-                "scheduler": ExponentialDecaySchedulerConfig(
-                    lr_final=0.0001, max_steps=200000
-                ),
+                # TODO: add scheduler config from nerf2mesh
+                # "scheduler": ExponentialDecaySchedulerConfig(
+                #     lr_final=0.0001, max_steps=200000
+                # ),
             }
         },
         vis="viewer",
-    )
+    ),
+    description="nerf2mesh",
 )
